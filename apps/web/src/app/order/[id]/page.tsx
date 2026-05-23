41ba3d90455da660d7adf8b71b2d365df7ae295b
SpySparsh <spyspring30@gmail.com>
2026-05-23 11:53:42 +0530

    Remove legacy frontend and server duplicates

--- apps/web/src/app/order/[id]/page.tsx
+++ apps/web/src/app/order/[id]/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import OrderDetail from "../../../legacy-pages/OrderDetail.jsx";
--- ecommerce-frontend/src/api/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  withCredentials: true // needed for refresh token cookie
-});
-export default instance;
